49d2b7a3164d70b1d49a0d0e23c77cc89ec53985
Vishal Malik <123573822+vishalmalik18@users.noreply.github.com>
2026-07-11 14:16:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from main import generate_response
 
+st.title("🤖 StadiumAssist AI")
+
+my_question = st.text_input("Enter your query")
+
+if my_question:
+    answer = generate_response(my_question)
+    st.write(answer)
